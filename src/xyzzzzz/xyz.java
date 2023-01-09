1535d1f08facc131cb2c0e5b4c59470f6fe7eae3
sararawashdeh <rawashdehsarah@hotmail.com>
2023-01-09 14:01:24 +0200

    update

--- src/xyzzzzz/xyz.java
+++ src/xyzzzzz/xyz.java
@@ -1,8 +1,14 @@
 package xyzzzzz;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Date;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -11,7 +17,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class xyz {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 
 WebDriver driver ; 
@@ -21,7 +27,6 @@ WebDriver driver ;
 		driver = new ChromeDriver();
 
 		driver.get("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login");
-		
 		
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
@@ -41,6 +46,33 @@ WebDriver driver ;
 		WebElement myInputForTheFirstName = driver
 				.findElement(By.xpath("/html/body/div/div/div[2]/div/div[2]/div/div/form/div[1]/input"));
 		myInputForTheFirstName.sendKeys(myListOfFirstName[randomOne]);
+
+		WebElement myInputForTheLastName = driver
+				.findElement(By.xpath("/html/body/div/div/div[2]/div/div[2]/div/div/form/div[2]/input"));
+
+		myInputForTheLastName.sendKeys(myListOfLastName[randomTwo]);
+
+		int PostalCodeRandomNumber = (int) (Math.random() * 10000);
+		String PostalCodeString = Integer.toString(PostalCodeRandomNumber);
+		driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[2]/div/div/form/div[3]/input"))
+		.sendKeys(PostalCodeString);
+		Date currentTime = new Date();
+
+		String myUpdatedate = currentTime.toString();
+
+		String NewDate = myUpdatedate.replace(":", "-");
+
+
+
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File file = ts.getScreenshotAs(OutputType.FILE);
+
+		FileUtils.copyFile(file, new File("./ScreenShot_Folder/" + NewDate + ".jpg"));
+
+		
+		
+		
+	
 		
 		
 	}
